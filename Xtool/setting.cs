a57b6d786549721ae15dec45ba9ecde3252ae230
ReyMax <ReyMax@DESKTOP-AUHV6S9>
2018-02-08 19:11:40 +0800

    1

--- Xtool/setting.cs
+++ Xtool/setting.cs
@@ -7,10 +7,10 @@ namespace WindowsFormsApplication1
 {
     public class setting
     {
-        public static string DataPath = Environment.CurrentDirectory + "\\data.mdb";
+        public static string DataPath = Environment.CurrentDirectory + @"\data.mdb";
         public static System.Windows.Forms.RichTextBox LogBox;
 
-        public static string TempDelTxtPath = Environment.CurrentDirectory + "\\Del";
-        public static string DelTxtPath = @"\\192.168.2.80\Xx\Del";
+        public static string TempDelTxtPath = Environment.CurrentDirectory + @"\Del";
+        public static string DelTxtPath = @"\\192.168.2.80\Raid\XX\Del";
     }
 }
